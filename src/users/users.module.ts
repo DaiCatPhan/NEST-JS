c312c9e4f7e7240cdca064ab04ab0ff10203e362
Phan Dai Cat <phandaicat12032002@gmail.com>
2023-10-02 14:19:13 +0700

    create new user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), // thuộc tính name là định danh 1 cái id ứng mới modal User, k phải trong bảng User
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
